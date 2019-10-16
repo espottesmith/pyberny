7ba1ff6df6c428d66f74a703399745a2bac54c31
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2019-10-16 14:58:03 -0700

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='berny',
+    version='0.1.1',
+    packages=['berny'],
+    url='github.com/jhrmnn/pyberny',
+    license='MIT',
+    author='Jan Hermann, Mark Williamson, Zhi Wang, Evan Spotte-Smith',
+    author_email='dev@jan.hermann.name',
+    description='Berny is an optimizer of molecular geometries with respect to the total energy, using nuclear gradient information.'
+)
